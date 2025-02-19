125658e92d21e59d99ae43849881d0598a192a31
Francisco Javier Navarrete Gonzalez <frnavarr@c2r4s1.42malaga.com>
2025-02-19 13:00:23 +0100

    makefile .h and algorithm.c

---
+++ Makefile
@@ -0,0 +1,9 @@
+
+NAME = push_swap
+
+SRCS = src/algorithm.c src/push_swap.c
+
+OBJS = ${SRCS:.c=.o}
+
+CC = CC
+CFLAGS = -Wall -Wextra -Werror
---
+++ includes/push_swap.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frnavarr <frnavarr@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/09 14:39:06 by frnavarr          #+#    #+#             */
+/*   Updated: 2025/02/19 12:56:33 by frnavarr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "../libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+typedef struct s_list
+{
+	int				value;
+	int				index;
+	struct s_list	*next;
+}
+
+#endif
