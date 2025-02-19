bf5ccf5b2be41197ccc0ab9083f06168c76855fa
Francisco Javier Navarrete Gonzalez <frnavarr@c2r4s1.42malaga.com>
2025-02-19 17:37:39 +0100

    avance

---
+++ src/swap.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frnavarr <frnavarr@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/19 13:04:30 by frnavarr          #+#    #+#             */
+/*   Updated: 2025/02/19 13:27:27 by frnavarr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	swap(t_list **stack)
+{
+	if(ft_lstsize(*stack) < 2)
+		return (-1);
+}
+
+int	sa(t_list **stack_a)
+{
+	if (swap(stack_a))
+}
+
+int	sb(t_list **stack_b)
+{
+	
+}
+
+int	ss(t_list **stack_a, t_list **stack_b)
+{
+	
+}
